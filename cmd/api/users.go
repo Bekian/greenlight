65a6f8dc5a30924b12803fe0a2a86724a4bd2f55
Bekian <Bekian.Cat@gmail.com>
2025-06-04 21:32:44 -0500

    feat(app): 14.3 Sending Activation Tokens
    here we modified the user welcome template to include the user activation token, and connected this up to our logic in the register user handler.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/Bekian/greenlight/internal/data"
 	"github.com/Bekian/greenlight/internal/validator"
@@ -58,9 +59,20 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
+		// string map to pass data into the template
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+			"userId":          user.ID,
+		}
 		// use the users email to send an email with the users data to the user.
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
