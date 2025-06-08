a499b76963f6b3f16792bed3ee2cfe8b886a2603
Bekian <Bekian.Cat@gmail.com>
2025-06-08 14:25:49 -0500

    feat(app): 16.3 Setting up the permissions model
    added perms model and a function to get all perms for a user

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,16 +13,18 @@ var (
 
 // model wrapper for easy autocomplete access
 type Models struct {
-	Movies MovieModel
-	Tokens TokenModel
-	Users  UserModel
+	Movies      MovieModel
+	Permissions PermissionsModel
+	Tokens      TokenModel
+	Users       UserModel
 }
 
 // constructor
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Tokens: TokenModel{DB: db},
-		Users:  UserModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionsModel{DB: db},
+		Tokens:      TokenModel{DB: db},
+		Users:       UserModel{DB: db},
 	}
 }
---
+++ internal/data/permissions.go
@@ -0,0 +1,63 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"slices"
+	"time"
+)
+
+// this holds all the perms for a user
+type Permissions []string
+
+// check if the user contains a specific perm by the perm's code
+func (p Permissions) Include(code string) bool {
+	return slices.Contains(p, code)
+}
+
+// define model type
+type PermissionsModel struct {
+	DB *sql.DB
+}
+
+// func to get all perm codes for a specific user
+// BEK Note: 16.3 again here i think this naming pattern is a bit odd, like why not GetAllPerms or GetUserPerms
+func (m PermissionsModel) GetAllForUser(userID int64) (Permissions, error) {
+	// pasted for correctness
+	query := `
+        SELECT permissions.code
+        FROM permissions
+        INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+        INNER JOIN users ON users_permissions.user_id = users.id
+        WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// execute query
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// declare a perms var to write perm values to
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+	// ensure no error when iterating over rows
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
